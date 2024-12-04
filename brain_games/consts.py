732ae801aa9846d7de5a2660a2b0b3bdd36100bb
malina-prog <dima.sidol16@google.com>
2024-12-04 03:13:03 +0300

    fix prime game

--- brain_games/consts.py
+++ brain_games/consts.py
@@ -5,6 +5,6 @@ MATH_SIGNS = ('+', '-', '*')
 EVEN_INSTRUCTION = 'Answer "yes" if the number is even, otherwise answer "no".'
 CALC_INSTRUCTION = 'What is the result of the expression?'
 GCD_INSTRUCTION = 'Find the greatest common divisor of given numbers.'
-PRIME_INSTRUCTION = 'Answer "yes" if given number is prime.'\
+PRIME_INSTRUCTION = 'Answer "yes" if given number is prime. '\
                     'Otherwise answer "no".'
 PROGRESSION_INSTRUCTION = 'What number is missing in the progression?'
